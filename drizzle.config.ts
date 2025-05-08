a84ca071a14c465f9d7db603f1320bd49050fde3
Kevin Jeremia <kejer48.kj@gmail.com>
2025-05-09 03:37:18 +0700

    fix drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,17 @@
-import type { Config } from "drizzle-kit";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-export default {
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
   schema: "./db/schema/*",
   out: "./db/migrations",
   dialect: "postgresql",
+  entities: {
+    roles: {
+      provider: "neon",
+    },
+  },
   dbCredentials: {
     // Pastikan environment variable ini ada di .env Anda
     host: process.env.DB_HOST!, // Ambil dari env
@@ -15,4 +21,4 @@ export default {
     database: process.env.DB_NAME!,
     ssl: process.env.DB_SSL === "true",
   },
-} satisfies Config;
+});
